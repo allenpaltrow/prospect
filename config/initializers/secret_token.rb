0ced6ab9e75292e60248e7c48434c6dfb2375226
Allen Paltrow <allen.paltrow@gmail.com>
2013-09-27 13:38:43 -0400

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prospect::Application.config.secret_key_base = '245286083c842b4e65711168dd48d167649067e04f87e836d90775ab60ce9e83b06343b6d7c4c978fe1170c3d83ee84461cf7a285251a869d947663bf211b41f'
