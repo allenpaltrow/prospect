87adc66d541d10ab0a72e2f6df2c7078b3fcc102
Allen Paltrow <allen.paltrow@gmail.com>
2013-10-03 09:10:06 -0400

    rspec generated

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Prospect::Application.config.secret_key_base = '245286083c842b4e65711168dd48d167649067e04f87e836d90775ab60ce9e83b06343b6d7c4c978fe1170c3d83ee84461cf7a285251a869d947663bf211b41f'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Prospect::Application.config.secret_key_base = secure_token
